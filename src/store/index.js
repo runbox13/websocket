5442eef73381e2ca8491a2778d4ebe79448178c1
toannguyen5017 <toannguyen5017@gmail.com>
2021-09-09 12:22:39 +1000

    add Redux extension tools to the createStore in order to debug more easilty

--- src/store/index.js
+++ src/store/index.js
@@ -14,6 +14,6 @@ const reducer = (state = initialState, action) => {
     return state;
 }
 
-const store = createStore(reducer);
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
