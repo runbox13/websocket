1885c48738cd952f7bba9310b9b35554f41de68c
Simon Nguyen <runbox13@hotmail.com>
2021-09-09 22:36:48 +1000

    add chatroom img, title and view

--- src/App.js
+++ src/App.js
@@ -4,87 +4,43 @@ import * as React from "react";
 import { render } from "react-dom";
 import c from "classnames";
 
-class Buttons extends React.Component {
-  state = {
-    like: 23,
-    dislike: 3,
-    likeActive: false,
-    dislikeActive: false
-  };
-
-  setDislike() {
-    this.setState({
-      dislikeActive: !this.state.dislikeActive,
-      dislike: this.state.dislikeActive
-        ? this.state.dislike - 1
-        : this.state.dislike + 1
-    });
-  }
-  setLike() {
-    this.setState({
-      likeActive: !this.state.likeActive,
-      like: this.state.likeActive ? this.state.like - 1 : this.state.like + 1
-    });
-  }
-
-  handleLike() {
-    if (this.state.dislikeActive) {
-      this.setLike();
-      this.setDislike();
-    }
-    this.setLike();
-  }
-
-  handleDislike() {
-    if (this.state.likeActive) {
-      this.setDislike();
-      this.setLike();
-    }
-    this.setDislike();
-  }
-
-  render() {
-    return (
-      <>
-        <button
-          onClick={() => this.handleLike()}
-          className={c({ ["active"]: this.state.likeActive })}
-        >
-          {this.state.like}
-        </button>
-        <button
-          className={c({ ["active"]: this.state.dislikeActive })}
-          onClick={() => this.handleDislike()}
-        >
-          {this.state.dislike}
-        </button>
-      </>
-    );
-  }
-}
+const rooms = [
+  {
+  img:'https://i.ytimg.com/vi/5qap5aO4i9A/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB6KAYW8BN9j5s-5nSSxs_g3DdfmA',
+  title:'lofi hip hop radio - beats to relax/study to',
+  views:'2000',
+},
+{
+  img:'https://i.ytimg.com/vi/XULUBg_ZcAU/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAGXaDCsrgDpwuqwE0ioASYvF1OCA',
+  title:'Calm Piano Music 24/7: study music, focus, think, meditation, relaxing music',
+  views:'1235',
+},
+{
+  img:'https://i.ytimg.com/vi/36YnV9STBqc/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBvMJHTmNfBKNe-cKpL_KjrhxXuvQ',
+  title:'The Good Life Radio • 24/7 Live Radio | Best Relax House, Chillout, Study, Running, Gym, Happy Music',
+  views:'1234',
+},
+];
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hello world
-        </a>
-        <Buttons/>;
-      </header>
-    </div>
+function Chatroom(){
+  return(
+    <section className='chatroom'>
+      {rooms.map((room) => {
+        const {img,title,views} = room;
+        return <Room room={room}></Room>;
+      })}
+    </section>
   );
 }
-
-
-export default App;
+const Room = (props) =>{
+  const {img,title,views} = props.room;
+  return(
+    <article className='chatroom'>
+      <img src={img}/>
+      <h1>{title}</h1>
+      <h4>{views} listeners</h4>
+    </article>
+  )
+};
+export default Chatroom;
