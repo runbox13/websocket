65bda5512299c09305ada3a5be55237914defd46
toannguyen5017 <toannguyen5017@gmail.com>
2021-10-01 15:40:46 +1000

    lobby placeholder

--- src/page/lobby.js
+++ src/page/lobby.js
@@ -1,10 +1,59 @@
+import axios from 'axios';
 import React from 'react'
 import { connect } from 'react-redux'
+import { Card, Button, CardText, CardTitle, CardBody } from 'reactstrap';
+
+
+
+
+function PlaceHolder(props) {
+    
+    var cards = [];
+
+    for(var i = 0; i < props.rooms.length; ++i){
+        cards.push(
+            <Card style={{width: '18rem'}}>
+                <CardBody>
+                    <CardTitle>
+                     {props.rooms[i].name}
+                    </CardTitle>
+                    <CardText>
+                        {props.rooms[i].description}
+                    </CardText>
+                    <Button variant="primary" href={"http://localhost:3000/" + "chatroom?id=" + props.rooms[i].id}>Go to Chatroom</Button>
+                </CardBody>
+            </Card>
+            );
+    }
+    
+    return(
+        cards
+    );
+}
 
 class Lobby extends React.Component {
+    componentDidMount() {
+        this.getRooms();
+    }
+
     constructor() {
         super()
-        this.state = {}
+        this.state = {
+            rooms: ''
+        }
+    }
+
+    getRooms = () => {
+        axios.get("http://localhost:8000/" + 'room')
+        .then(data => {
+            this.setState( {
+                rooms: data.data
+            })
+            console.log(this.state[0]);
+            console.log(this.state[0].name);
+        }).catch(error => {
+            console.log(error);
+        });
     }
 
     render() {
@@ -18,6 +67,9 @@ class Lobby extends React.Component {
                             {this.props.user.display_name}
                     </code>
                 </p>
+                <PlaceHolder api={this.props.api} rooms={this.state.rooms}>
+
+                </PlaceHolder>
             </div>
         );
     }
@@ -35,6 +87,7 @@ const mapDispatchToProps = dispatch => {
         dispatch
     }
 }
+//
 
 export default connect(
     mapStateToProps,
