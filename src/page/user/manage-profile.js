340906fb53083996348dad33394705e144d47588
peefeeyatko <marshall.anthonys@gmail.com>
2021-10-04 19:28:18 +1100

    hook up PUT and DELETE requests to API

--- src/page/user/manage-profile.js
+++ src/page/user/manage-profile.js
@@ -5,8 +5,7 @@ import {
     FormGroup,
     Label,
     Input,
-    Button,
-    Alert
+    Button
 } from 'reactstrap';
 import axios from 'axios';
 import { FormErrors } from '../../FormErrors';
@@ -15,18 +14,15 @@ import { FormErrors } from '../../FormErrors';
 class ManageProfile extends React.Component {
     constructor() {
         super()
-        this.state =
-        {
+        this.state = {
             // Initialise empty state to pass user data.
             id: null,
             email: '',
             display_name: '',
-            password: '',
             bio: '',
             avatar: null,
             formErrors: { email: '' },
             emailValid: true,
-            passwordValid: true,
             displaynameValid: true,
             bioValid: true
         }
@@ -41,7 +37,6 @@ class ManageProfile extends React.Component {
     componentDidMount() {
         // Pass user into state if component runs
         this.fetchUser();
-        console.log(this.props.user)
     }
 
     // Fetch user from redux store.
@@ -49,8 +44,8 @@ class ManageProfile extends React.Component {
         this.setState({
             email: this.props.user.email,
             display_name: this.props.user.display_name,
-            password: this.props.user.password,
-            bio: this.props.user.bio
+            bio: this.props.user.bio,
+            avatar: this.props.user.avatar
         })
     }
 
@@ -58,7 +53,6 @@ class ManageProfile extends React.Component {
     validateField(fieldName, value) {
         let fieldValidationErrors = this.state.formErrors;
         let emailValid = this.state.emailValid;
-        let passwordValid = this.state.passwordValid;
         let displaynameValid = this.state.displaynameValid;
         let bioValid = this.state.bioValid;
 
@@ -70,10 +64,6 @@ class ManageProfile extends React.Component {
             case 'display_name':
                 displaynameValid = value.length >= 5;
                 fieldValidationErrors.display_name = displaynameValid ? '' : ' is too short, minimum 5 letters.';
-                break;
-            case 'password':
-                passwordValid = value.length >= 5;
-                fieldValidationErrors.password = passwordValid ? '' : ' is too short, minimum 5 letters.';
                 break;
             case 'bio':
                 bioValid = value.length >= 20;
@@ -90,11 +80,10 @@ class ManageProfile extends React.Component {
 
     // Whether form is valid or not. 
     validateForm() {
-        this.setState({ formValid: this.state.emailValid && this.state.passwordValid && this.state.displaynameValid && this.state.bioValid });
+        this.setState({ formValid: this.state.emailValid && this.state.displaynameValid && this.state.bioValid });
     }
 
     handleChange(event) {
-        //console.log(event)
         const target = event.target
         const value = target.value
         const name = target.name
@@ -110,23 +99,6 @@ class ManageProfile extends React.Component {
         })
     }
 
-    handleUpdate(e) {
-        axios.put('http://localhost:5000/users/1', {
-            email: this.state.email,
-            display_name: this.state.display_name,
-            password: this.state.password,
-            bio: this.state.bio
-        })
-            .then(() => {
-                alert("Profile updated!")
-                this.props.history.push('/manage-profile')
-            }).catch(error => {
-                alert(error)
-                console.log(error)
-            })
-        e.preventDefault();
-    }
-
     // Reset state to user info in redux store.
     handleReset() {
         if (window.confirm("Are you sure you want to reset all changes?")) {
@@ -134,24 +106,25 @@ class ManageProfile extends React.Component {
         }
     }
 
-    handleDelete() {
-        if (window.confirm("Are you sure you want to remove your profile?")) {
-            this.props.history.push('/')
-        }
-    }
-
-    /** 
-    For Marshall's backend API.
-
     handleUpdate(e) {
-        axios.put('this.props.api + 'user/' + this.props.user.id, {
+        axios.put(this.props.api + 'user/' + this.props.user.id, {
             email: this.state.email,
             display_name: this.state.display_name,
-            password: this.state.password,
-            bio: this.state.bio
+            bio: this.state.bio,
+            avatar: this.state.avatar
         })
-            .then(() => {
+            .then((response) => {
+
+                // dispatch USER_SESSION action to save user data to redux store
+                this.props.dispatch({
+                    type: 'USER_SESSION',
+                    payload: response.data.user
+                })
+                
+                // reload component
                 this.props.history.push('/manage-profile')
+                
+                // confirm to user profile details are updated..
             }).catch(error => {
                 alert(error)
                 console.log(error)
@@ -159,20 +132,19 @@ class ManageProfile extends React.Component {
         e.preventDefault();
     }
     
-    handleDelete(id, index) {
-        if (window.confirm("Are you sure you want to delete this room?")) {
+    handleDelete() {
+        if (window.confirm("Are you sure you want to delete your profile?")) {
             axios.delete(this.props.api + 'user/' + this.props.user.id)
                 .then((response) => {
                     console.log(response)
-                    this.props.history.push('/')
+                    window.location.href = '/?account-deleted'
                 });
         }
     }
-    */
 
     render() {
         return (
-            <div className="container main">
+            <div className="container main manage-profile">
 
                 <h1 className="mb-4">Manage Profile</h1>
 
@@ -202,17 +174,7 @@ class ManageProfile extends React.Component {
                     </FormGroup>
 
                     <FormGroup>
-                        <Label for="password">Password:</Label>
-                        <Input type="password"
-                            name="password"
-                            id="password"
-                            value={this.state.password}
-                            onChange={this.handleChange}
-                            required />
-                    </FormGroup>
-
-                    <FormGroup>
-                        <Label for="bio">Description:</Label>
+                        <Label for="bio">Bio:</Label>
                         <Input type="textarea"
                             name="bio"
                             id="bio"
@@ -231,7 +193,7 @@ class ManageProfile extends React.Component {
                     </FormGroup>
 
                     <FormGroup>
-                        <img src={this.state.avatar} width="300" height="300" alt="" />
+                        <img src={this.state.avatar} width="200" height="200" alt="" />
                     </FormGroup>
 
 
