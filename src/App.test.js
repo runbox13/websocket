bf0d34c16ba37c3c27d93bf691b6a2b819a23815
peefeeyatko <marshall.anthonys@gmail.com>
2021-10-04 14:48:18 +1100

    get test passing again

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App'
+import store from './store/index'
+import { Provider } from 'react-redux'
+import { render, screen } from '@testing-library/react'
+
+const Wrapper = ({ children }) => (
+  // As your component uses your Redux state, you need to wrap it with a Provider
+  // https://stackoverflow.com/questions/68822417
+  <Provider store={store}>{children}</Provider>
+)
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  render(<App />, { wrapper: Wrapper })
+  const linkElement = screen.getByText(/learn react/i)
+  expect(linkElement).toBeInTheDocument()
+})
