7dd081f8681a36d42b20093d4f812653664e9787
peefeeyatko <marshall.anthonys@gmail.com>
2021-10-04 16:01:21 +1100

    fix build, modify playlist object to include title and description

--- src/page/user/manage-playlist.js
+++ src/page/user/manage-playlist.js
@@ -1,43 +1,49 @@
 import React from 'react'
 import { connect } from 'react-redux'
-//import api from "../../api/playlist.js"
 import axios from 'axios';
-import playlist from '../../api/playlist';
 
 class ManagePlaylist extends React.Component {
     constructor() {
         super()
         this.state = { 
             //isLoaded: false,
-            playlist: [
-                {
-                    "id":"1",
-                    "song_title":"What is Love?",
-                    "artist":"Twice",
-                    "link":"https://www.youtube.com/watch?v=i0p1bmr0EmE"
-                },
-                
-                {
-                    "id":"2",
-                    "song_title":"Every Summertime",
-                    "artist":"Niki",
-                    "link":"https://www.youtube.com/watch?v=OXtZfPZIex4"
-                }
-            ]
+            playlist: {
+                title: "A sample playlist",
+                description: "Lorem ipsum dolor sit amet..",
+                tracks: [
+                    {
+                        "id": 1,
+                        "song_title": "What is Love?",
+                        "artist": "Twice",
+                        "link": "https://www.youtube.com/watch?v=i0p1bmr0EmE"
+                    },
+                    {
+                        "id": 2,
+                        "song_title": "Every Summertime",
+                        "artist": "Niki",
+                        "link": "https://www.youtube.com/watch?v=OXtZfPZIex4"
+                    }
+                ]
+            }
         }
     }
 
     componentDidMount() {
-        axios.get(this.props.api + 'song/' + this.props.user.id)
+        // Fetch playlist data and pass into empty state
+        axios.get(this.props.api + 'playlist', {
+        })
             .then((response) => {
+                console.log(response.data)
                 this.setState({
-                    isLoaded: true,
-                    playlist: response.data
+                    song_title: response.data.song_title,
+                    artist: response.data.artist,
+                    link: response.data.link,
                 })
-            }).catch(error => {
+            })
+            .catch((error) => {
                 alert(error)
-                console.log(error)
-            });
+                console.log(error);
+            })
     }
 
     handleDelete(id, index) {
@@ -62,56 +68,38 @@ class ManagePlaylist extends React.Component {
         this.props.history.push(path)
     }
 
-    // Retrieve playlist contents
-    fetchPlaylist() {
-        // Fetch playlist data and pass into empty state
-        axios.get('http://localhost:5000/playlist', {
-        })
-            .then((response) => {
-                console.log(response.data)
-                this.setState({
-                    song_title: response.data.song_title,
-                    artist: response.data.artist,
-                    link: response.data.link,
-                })
-            })
-            .catch((error) => {
-                alert(error)
-                console.log(error);
-            })
-    }
-
     render() {
         return (
             <div className="container main" >
-                <h1>Manage Playlist</h1>
-                <br></br>
-                <h2>Playlist: Sample</h2>
+                <h1 className="mb-4">Manage Playlist</h1>
 
-                <div class="align-middle">
-                <table class="table table-striped" class="table table-responsive">
+                <h2>{this.state.playlist.title}</h2>
+                <p>{this.state.playlist.description}</p>
+
+                <div className="align-middle">
+                <table className="table table-responsive">
                     <thead>
                         <tr>
-                        <th scope="col-auto">#</th>
-                        <th scope="col-auto">Song Title</th>
-                        <th scope="col-auto">Artist</th>
-                        <th scope="col-auto">Link</th>
-                        <th scope="col-auto">Options</th>
+                            <th scope="col-auto">#</th>
+                            <th scope="col-auto">Song Title</th>
+                            <th scope="col-auto">Artist</th>
+                            <th scope="col-auto">Link</th>
+                            <th scope="col-auto">Options</th>
                         </tr>
                     </thead>
                     
                     <tbody>
-                    {this.state.playlist.map((playlist, i) => (
-                        <tr className="align-middle" key={playlist.id}>
+                    {this.state.playlist.tracks.map((track, i) => (
+                        <tr className="align-middle" key={track.id}>
                             <th scope="row">{ i + 1 }</th>
-                            <td className="align-middle">{playlist.song_title}</td>
-                            <td className="align-middle">{playlist.artist}</td>
-                            <td className="align-middle">{playlist.link}</td>
-                            <td className="align-middle" className="text-center">
+                            <td className="align-middle">{track.song_title}</td>
+                            <td className="align-middle">{track.artist}</td>
+                            <td className="align-middle">{track.link}</td>
+                            <td className="align-middle text-center">
                                 <button type="button" className="btn btn-outline-secondary" 
-                                    onClick={() => this.handleEdit(playlist.id)}>Edit</button>
+                                    onClick={() => this.handleEdit(track.id)}>Edit</button>
                                 <button type="button" className="btn btn-danger" 
-                                    onClick={() => this.handleDelete(playlist.id, i)}>Delete</button>
+                                    onClick={() => this.handleDelete(track.id, i)}>Delete</button>
                             </td>
                         </tr>
                     ))}
