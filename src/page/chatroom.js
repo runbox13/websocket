68d1e64fdaa263620f0c2f15770a5948c8354622
toannguyen5017 <toannguyen5017@gmail.com>
2021-10-05 14:53:43 +1100

    finish queue functionalities

--- src/page/chatroom.js
+++ src/page/chatroom.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Container, Row, Col, Card, Navbar, NavbarBrand, Button } from 'reactstrap';
 import ReactPlayer from 'react-player'
-import {w3cwebsocket as W3CWebSocket} from 'websocket'; 
-import {connect} from 'react-redux';
+import { w3cwebsocket as W3CWebSocket } from 'websocket';
+import { connect } from 'react-redux';
 import axios from 'axios';
 
 
 const socket = new W3CWebSocket('ws://127.0.0.1:8080');
 
 function SidebarPlaylist() {
-    return(
+    return (
         <div className="sideBarPlaylist">
             <p>Playlists</p>
         </div>
@@ -17,38 +17,38 @@ function SidebarPlaylist() {
 }
 
 function SideBarChatbox(props) {
-    
-        return (
+
+    return (
         <div className="sideBarChatbox">
             <p>Chatbox</p>
-        <ul>
-            <UsersList users={props.state.users}></UsersList>
-        </ul>
-    </div>)
+            <ul>
+                <UsersList users={props.state.users}></UsersList>
+            </ul>
+        </div>)
 }
 
 function UsersList(props) {
 
     var array = [];
 
-    for(var key in props.users) {
+    for (var key in props.users) {
         array.push(<li id={key}>{props.users[key].display_name}</li>);
     }
-    
+
     return array;
 
 }
 
 function CenterChatroom() {
-    return(
+    return (
         <div className="centerChatroom">
             <div className="video-wrapper">
-                <ReactPlayer  url='' playing={true} controls={true} width={"100%"} height={"100%"}/>
-           </div>
-           <Card>
-               <Card body> Rick Roll
-               </Card>
-           </Card>
+                <ReactPlayer url='' playing={true} controls={true} width={"100%"} height={"100%"} />
+            </div>
+            <Card>
+                <Card body> Rick Roll
+                </Card>
+            </Card>
         </div>
     );
 }
@@ -59,7 +59,7 @@ function waitForSocketConnection(socket, callback) {
         function () {
             if (socket.readyState === 1) {
                 console.log("Connection is made")
-                if (callback != null){
+                if (callback != null) {
                     callback();
                 }
             } else {
@@ -69,22 +69,22 @@ function waitForSocketConnection(socket, callback) {
         }, 5);
 }
 
-function joinChatroom(user, room) {  
-    waitForSocketConnection(socket, function() {
+function joinChatroom(user, room) {
+    waitForSocketConnection(socket, function () {
         socket.send(JSON.stringify({
             type: "message",
             messageType: "join",
             user: user,
             room: room
         }));
-    
+
         console.log("Request sent!");
     })
- }  
+}
 
 
 class Chatroom extends React.Component {
-    
+
     componentDidMount() {
         socket.onopen = () => {
             console.log("WS connected");
@@ -93,14 +93,14 @@ class Chatroom extends React.Component {
         var roomId = (window.location.href).split("?id=")[1];
 
         axios.get(this.props.api + "room/" + roomId)
-        .then(payload => {
-            this.setState(prevState => {
-                var newState = Object.assign(prevState);
-                newState.room = payload.data;
-                return newState;
-            })     
-            joinChatroom(this.props.user, this.state.room);
-        }).catch(error => {console.log(error)});
+            .then(payload => {
+                this.setState(prevState => {
+                    var newState = Object.assign(prevState);
+                    newState.room = payload.data;
+                    return newState;
+                })
+                joinChatroom(this.props.user, this.state.room);
+            }).catch(error => { console.log(error) });
     }
 
     componentWillUnmount() {
@@ -109,15 +109,17 @@ class Chatroom extends React.Component {
 
     constructor() {
         super();
-         this.state = {
-             users: {},
-             room: {},
-             queue: {},
-             isQueued: false
-         }
+        this.state = {
+            users: {},
+            room: {},
+            queue: [],
+            isQueued: false,
+            dj: [],
+            isDJ: false
+        }
 
         socket.onclose = event => {
-            
+
         };
 
         socket.onmessage = event => {
@@ -125,47 +127,69 @@ class Chatroom extends React.Component {
 
             var messageEvent = JSON.parse(event.data);
             var users;
-            if(messageEvent.type === "getList") {
+
+            //Set the DJ state
+            if (messageEvent.type == "setDj") {
+                //Set DJ state from websocket data
+                this.setState(prevState => { return { dj: messageEvent.payload } });
+                //If this user is the DJ, set the booleans
+                if (this.state.dj != null && this.state.dj.id === this.props.user.id) {
+                    this.setState({ isDJ: true });
+                    this.setState({ isQueued: false });
+                } else {
+                    this.setState({ isDJ: false });
+                }
+
+            }
+
+            if (messageEvent.type === "getList") {
                 users = messageEvent.payload;
-                    
-                if(messageEvent.action == "ADD") {
+
+                if (messageEvent.action == "ADD") {
                     this.setState(prevState => {
                         var newState = prevState;
                         newState.users = users;
                         return newState;
                     })
                 }
-                
-                if(messageEvent.action == "DELETE") {
+
+                if (messageEvent.action == "DELETE") {
                     this.setState(prevState => {
                         var newState = prevState;
-                        newState.users = users;                 
+                        newState.users = users;
                         return newState;
                     })
                 }
-            } 
-            
-            if(messageEvent.type == "getQueue") {
-                console.log("queue");
+            }
+
+            //Receive queue, update state
+            if (messageEvent.type == "getQueue") {
                 var queue = messageEvent.payload;
                 this.setState(prevState => {
                     var newState = prevState;
                     newState.queue = queue;
                     return newState;
                 });
+                //If queue array contains a user with the same id, set boolean
+                for (var key in this.state.queue) {
+                    if (this.props.user.id == this.state.queue[key].id) this.setState({ isQueued: true });
+                    else this.setState({ isQueued: false });
+                }
+
+                //If the queue array is empty, set boolean
+                if (this.state.queue.length === 0) this.setState({ isQueued: false });
             }
-    };
-  }
+        };
+    }
 
     queueDj = () => {
         socket.send(JSON.stringify({
             type: "message",
             messageType: "queue",
             action: "ADD",
-            payload: this.props.user,
+            user: this.props.user,
             roomId: this.state.room.id
         }));
-        this.setState({isQueued: true});
     }
 
     deQueue = () => {
@@ -173,50 +197,57 @@ class Chatroom extends React.Component {
             type: "message",
             messageType: "queue",
             action: "DELETE",
-            payload: this.props.user,
+            user: this.props.user,
             roomId: this.state.room.id
         }));
-        this.setState({isQueued: false});
     }
 
-  
+
+    jumpOffDj = () => {
+        socket.send(JSON.stringify({
+            type: "message",
+            messageType: "jumpOff",
+            roomId: this.state.room.id
+        }));
+    }
 
     render() {
-    return(
-        <>
-        <Container fluid>
-            <Navbar>
-                   <NavbarBrand>{this.state.room.name}</NavbarBrand>
-                   <Button onClick={this.state.isQueued ? this.deQueue : this.queueDj}> {this.state.isQueued ? 'Dequeue' : 'Queue up for DJ'}</Button>
-            </Navbar>
-            <Row>
-                <Col>
-                    <SidebarPlaylist/>
-                </Col>
-                <Col xs={8}>
-                    <CenterChatroom/>
-                </Col>
-                <Col>
-                    <SideBarChatbox state={this.state}/>
-                </Col>
-            </Row>
-        </Container>
-        </>
-    );
-};
+        return (
+            <>
+                <Container fluid>
+                    <Navbar>
+                        <NavbarBrand>{this.state.room.name}</NavbarBrand>
+                        {!this.state.isDJ ? <Button onClick={this.state.isQueued ? this.deQueue : this.queueDj}> {this.state.isQueued ? 'Dequeue' : 'Queue up for DJ'}</Button>
+                            : <Button onClick={this.jumpOffDj}>Jump off</Button>}
+                    </Navbar>
+                    <Row>
+                        <Col>
+                            <SidebarPlaylist />
+                        </Col>
+                        <Col xs={8}>
+                            <CenterChatroom />
+                        </Col>
+                        <Col>
+                            <SideBarChatbox state={this.state} />
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        );
+    };
 }
 
 const mapStateToProps = state => {
-  return { 
-      user: state.user,
-      api: state.api
-  }
+    return {
+        user: state.user,
+        api: state.api
+    }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
         dispatch
     }
-  }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(Chatroom);
--- src/websocket.js
+++ src/websocket.js
@@ -15,85 +15,129 @@ const connections = {
 console.log("Server running");
 
 
-wsServer.on('request', function(request) {
-    const connection = request.accept(null, request.origin); 
-    
-    connection.on('message', function(message) {
-            var payload = JSON.parse(message.utf8Data);
-            
-            if(payload.messageType === "queue") {
-                if(payload.action === "DELETE") {
-                    var index = connections[payload.roomId].queue.indexOf(payload.user);
-                    connections[payload.roomId].queue.splice(index, 1);
-                } else {
-                    connections[payload.roomId].queue.push(payload.payload);
-                }
-                for(var key in connections[payload.roomId].users) {
-                    connections[payload.roomId].users[key].connection.sendUTF(JSON.stringify({
-                        type: "getQueue",
-                        action: "ADD",
-                        payload: connections[payload.roomId].queue
-                    }));
-                }
-                
-                console.log(connections[payload.roomId].queue);
-                console.log(payload.roomId);
+
+
+wsServer.on('request', function (request) {
+    const connection = request.accept(null, request.origin);
+
+    connection.on('message', function (message) {
+        var payload = JSON.parse(message.utf8Data);
+        //Set a DJ from the queue
+        var setDJ = (room, type) => {
+            room.dj = room.queue.pop();
+
+            for (var key in room.users) {
+                room.users[key].connection.sendUTF(JSON.stringify({
+                    type: "setDj",
+                    payload: room.dj
+                }));
             }
+        }
 
-            if(payload.messageType === "join") {
-                var room = payload.room;
-                var user = payload.user;
+        //If the DJ jumps off, set the next DJ
+        if (payload.messageType === "jumpOff") {
+            var room = connections[payload.roomId];
+            setDJ(room);
 
-                //assume connection exists
-                if(!connections.hasOwnProperty([room.id]))
+            //Send every user in room the new queue list
+            for (var key in room.users) {
+                room.users[key].connection.sendUTF(JSON.stringify({
+                    type: "getQueue",
+                    payload: room.queue
+                }));
+            }
+        }
+
+        //Queue - Add, delete
+        if (payload.messageType === "queue") {
+            var room = connections[payload.roomId];
+            //Delete user from array
+            if (payload.action === "DELETE") {
+                var index = room.queue.indexOf(payload.user);
+                room.queue.splice(index, 1);
+            } else { //Add user to queue array if user isn't already the dj
+                if (payload.user !== room.dj) {
+                    //If a DJ exists check if the user ID matches, if it doesn't add to queue
+                    if (room.dj != null) {
+                        if (room.dj.id !== payload.user.id) {
+                            room.queue.push(payload.user);
+                        }
+                    }
+                    else {// add user to queue if the DJ doesn't match
+                        room.queue.push(payload.user);
+                        setDJ(room);
+                    }
+                }
+            }
+            //Send every user in room the new queue list
+            for (var key in room.users) {
+                room.users[key].connection.sendUTF(JSON.stringify({
+                    type: "getQueue",
+                    payload: room.queue
+                }));
+            }
+        }
+
+
+        //When user joins
+        if (payload.messageType === "join") {
+            var room = payload.room;
+            var user = payload.user;
+
+            //assume connection exists
+            if (!connections.hasOwnProperty([room.id]))
                 connections[room.id] = {
                     users: {},
                     room: room,
-                    dj: "",
+                    dj: null,
+                    //Array of user objects to use queue pop/push functionalities
                     queue: []
                 }
 
-                connections[room.id].users[user.id] = {
-                    connection: connection,
-                    user: user
-                }
+            connections[room.id].users[user.id] = {
+                connection: connection,
+                user: user
+            }
 
-                console.log("User: " + user.display_name + " joined chatroom: " + room.name);
+            console.log("User: " + user.display_name + " joined chatroom: " + room.name);
 
-                console.log(connections[room.id].users[user.id].user.id + " connected to " + 
+            console.log(connections[room.id].users[user.id].user.id + " connected to " +
                 connections[room.id].room.id);
 
-                var sendList = (action) => {
-                    var users = {};
+            //Send list of users to all users when a user joins
+            var sendList = (action) => {
+                var users = {};
 
-                    for(key in connections[room.id].users) {
-                        users[connections[room.id].users[key].user.id] = connections[room.id].users[key].user;
-                    }
-
-                    for(key in connections[room.id].users) {
-                        connections[room.id].users[key].connection.sendUTF(JSON.stringify(
-                            {
-                                type: "getList",
-                                payload: users,
-                                action: action,
-                            }))
-                    }
+                for (key in connections[room.id].users) {
+                    users[connections[room.id].users[key].user.id] = connections[room.id].users[key].user;
                 }
-                sendList("ADD");
 
-                connection.on('close', message => {
-                    delete connections[room.id].users[payload.user.id];
-                    if(Object.keys(connections[room.id].users).length == 0) {
-                        delete connections[room.id];
-                    }
-                    else sendList("DELETE");
-                    console.log(connections);
-                });     
-                
-                
+                for (key in connections[room.id].users) {
+                    connections[room.id].users[key].connection.sendUTF(JSON.stringify(
+                        {
+                            type: "getList",
+                            payload: users,
+                            action: action,
+                        }))
+                }
             }
-            
-            
+
+            //When user joins, send all users the list of users
+            sendList("ADD");
+
+            connection.on('close', message => {
+                delete connections[room.id].users[payload.user.id];
+                if (Object.keys(connections[room.id].users).length == 0) {
+                    delete connections[room.id];
+                }
+                else sendList("DELETE");
+                console.log(connections);
+            });
+
+
+        }
+
+
     })
 
 });
