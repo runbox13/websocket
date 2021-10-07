030285c2ef16d007ebd33005c6be03c5081a9ec4
Simon Nguyen <runbox13@hotmail.com>
2021-10-07 16:43:00 +1100

    reformat lobby code structure

--- src/page/lobby.js
+++ src/page/lobby.js
@@ -2,66 +2,61 @@ import React from 'react';
 import { connect } from 'react-redux';
 import './lobby.css';
 import axios from 'axios';
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { Card, Button, CardText, CardTitle, CardBody, CardSubtitle, CardImgOverlay, CardImg} from 'reactstrap';
 
-class Lobby extends React.Component {
-    constructor() {
-        super()
-        this.state = {}
-    }
+function App() {
+  const [rooms, setPost] = React.useState([]);
+  const [users, setUsers] = React.useState([]);
+  const api = useSelector(state => state.api);
+  const user = useSelector(state => state.user);
+  const history = useHistory()
+  const dispatch = useDispatch();
 
-    render() {
-        return (
-            <div className="container main">
-                <h1>Lobby</h1>
-                <p>
-                    Hey there, <code
-                        className="code-link"
-                        onClick={() => this.props.history.push('/profile?id=' + this.props.user.id)}>
-                            {this.props.user.display_name}
-                        
-                    </code>
-                </p>
-                
-                <div>{Chatroom()}</div>
-            </div>
-        );
-    }
-}
+  React.useEffect(() => {
+    axios.get("http://localhost:8000/user").then((response) => {
+      setUsers(response.data);
+    });
+    axios.get("http://localhost:8000/room").then((response) => {
+      setPost(response.data);
+    });
+  }, []);
 
-const rooms = [
-  {id:1,
+const pic = [
+{id:1,
   img:'https://i.ytimg.com/vi/5qap5aO4i9A/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB6KAYW8BN9j5s-5nSSxs_g3DdfmA',
-  title:'lofi hip hop radio - beats to relax/study to',
-  views:'2000',
 },
 {id:2,
   img:'https://i.ytimg.com/vi/XULUBg_ZcAU/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAGXaDCsrgDpwuqwE0ioASYvF1OCA',
-  title:'Calm Piano Music 24/7: study music, focus, think, meditation, relaxing music',
-  views:'1235',
 },
 {id:3,
   img:'https://i.ytimg.com/vi/36YnV9STBqc/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBvMJHTmNfBKNe-cKpL_KjrhxXuvQ',
-  title:'The Good Life Radio • 24/7 Live Radio | Best Relax House, Chillout, Study, Running, Gym, Happy Music',
-  views:'1234',
 },
 {id:4,
   img:'https://i.ytimg.com/vi/62d2QvWAVt4/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCgA-azV2AUnoCVrmd_stl_FHMqrQ',
-  title:'Music to put you in a better mood ~ A playlist lofi for study, relax, stress relief',
-  views:'1234',
 },
 {id:5,
   img:'https://i.ytimg.com/vi/5qap5aO4i9A/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB6KAYW8BN9j5s-5nSSxs_g3DdfmA',
-  title:'lofi hip hop radio - beats to relax/study to',
-  views:'2000',
 },
 {id:6,
-  img:'https://i.ytimg.com/vi/XULUBg_ZcAU/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAGXaDCsrgDpwuqwE0ioASYvF1OCA',
-  title:'Calm Piano Music 24/7: study music, focus, think, meditation, relaxing music',
-  views:'1235',
+  img:'https://i.ytimg.com/vi/2QXa9ib4dbA/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDAoUNQOZ2ZXG_7TalFGbn7ItbSFQ',
 },
-];
+{id:7,
+  img:'https://i.ytimg.com/vi/kovfGjs7d0Q/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA8wMnXDaVF_SeheBVLCv3a6TbvBQ',
+},
+{id:8,
+  img:'https://i.ytimg.com/vi/4zeUOUo09Hs/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCRzd6bunDkU_IS_IWKByYz-bxXyQ',
+},
+{id:9,
+  img:'https://i.ytimg.com/vi/n3rzL9JLp8o/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA-lnn1rSCLf0kCuVzaOgd0UDchiA',
+},
+{id:10,
+  img:'https://i.ytimg.com/vi/XULUBg_ZcAU/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAGXaDCsrgDpwuqwE0ioASYvF1OCA',
+},
+];  
 
- function Chatroom(){
+function Chatroom(){
   return(
     <section className='flex-container'>
       {rooms.map((room) => {
@@ -70,36 +65,44 @@ const rooms = [
     </section>
   )
 }
-    
-const Room = (props) =>{
-  const {id,img,title,views} = props;
 
+const Room = (props) =>{
+  const {id,name,description,user_id} = props;
   return(
-    <article className='chatroom'>
-      <a className = "limit" href={"http://localhost:3000/" + "chatroom?id=" + id}><img src={img} alt="chatroom"/><span className="text" title={title}>{title}</span>
-</a>
-      <h4>{views} listeners</h4>
-    </article>
+    
+      <Card  border="danger" style={{ width: '18rem' }}>
+        <a href={"http://localhost:3000/chatroom?id=" + id} >
+           <CardImg variant="top" src={pic[id].img} />
+                </a>
+                <CardBody>
+                    <CardTitle> 
+                     {name}
+                    </CardTitle>
+                    <CardSubtitle className="mb-2 text-muted"><p>Created by, <a className="mb-2 text-muted" href={"http://localhost:3000/profile?id=" + id}>{users[user_id-1].display_name}</a></p></CardSubtitle>
+                    <CardText>
+                        {description}
+                    </CardText>
+                </CardBody>
+    
+        </Card>
+      
   );
 };
 
+  return (
+    <div className="container main">
+                <h1>Lobby</h1>
+                <p>
+                    Hey there, <code
+                        className="code-link"
+                        onClick={() => history.push('/profile?id=' + user.id)}>
+                            {user.display_name}
+                    </code>
+                </p>
+                 <div>{Chatroom()}</div> 
+            </div>
+  );
+}
 
 
-
-const mapStateToProps = state => {
-    return { 
-        api: state.api,
-        user: state.user
-    }
-};
-
-const mapDispatchToProps = dispatch => {
-    return {
-        dispatch
-    }
-};
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Lobby);
+export default connect()(App);
