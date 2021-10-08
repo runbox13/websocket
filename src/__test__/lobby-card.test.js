979e68ea53f10e849230733679f821899b54a156
toannguyen5017 <toannguyen5017@gmail.com>
2021-10-08 14:13:27 +1100

    add comments

--- src/__test__/lobby-card.test.js
+++ src/__test__/lobby-card.test.js
@@ -5,7 +5,7 @@ import '@testing-library/jest-dom/extend-expect';
 import { Provider } from 'react-redux';
 import { store, persistor } from '../store/index.js';
 import { PersistGate } from 'redux-persist/integration/react';
-
+//Wrapper to wrap arouund the Lobby component during render
 const Wrapper = ({ children }) => (
   <Provider store={store}>
     <PersistGate persistor={persistor}>
@@ -14,6 +14,7 @@ const Wrapper = ({ children }) => (
   </Provider>
 );
 
+//Render Lobby and test to see if lobby cards is shown on the screen
 test("Lobby cards loaded in ", () => {
     const component = render(<Lobby/>, { wrapper: Wrapper })
     const cardEle = component.getByTestId("cardHeader")
--- src/__test__/manage-profile.test.js
+++ src/__test__/manage-profile.test.js
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux';
 import { store, persistor } from '../store/index.js';
 import { PersistGate } from 'redux-persist/integration/react';
 
+//Wrapper to wrap arouund the ManageProfile component during render
 const Wrapper = ({ children }) => (
   <Provider store={store}>
     <PersistGate persistor={persistor}>
@@ -14,6 +15,7 @@ const Wrapper = ({ children }) => (
   </Provider>
 );
 
+//Render ManageProfile and test to see if "Manage Profile" is shown on the screen
 test("header renders with correct text", () => {
     const component = render(<ManageProfile />, { wrapper: Wrapper })
     const headerEle = component.getByTestId("manageProfileHeader")
