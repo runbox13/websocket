f1b8c8975271bc305abc5c5d8fa5580399476440
simon <39092572+runbox13@users.noreply.github.com>
2021-10-28 23:36:35 +1100

    Update lobby.js

--- src/page/lobby.js
+++ src/page/lobby.js
@@ -67,7 +67,7 @@ function Lobby() {
       <p>
         Hey there, <a
           className="user-link"
-          href={'/profile?id=' + user.id'}>
+          href={'/profile?id=' + user.id}>
           {user.display_name}
         </a>
       </p>
